3a2a7a59bb5b6c27bd4642c2de0149c64164d0b3
SASI ANANTH K <sasiananth5044@gmail.com>
2026-09-23 10:30:19 +0530

    Add calculator functions to script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+let display = document.getElementById("display");
 
+function add(value) {
+    display.value += value;
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch {
+        display.value = "Error";
+    }
+}
